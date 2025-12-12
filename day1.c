60c09d03b1e07bddb9a98c5059008887c3dc8fe1
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-12-12 16:05:00 +0530

    Day1 Notes and Code Divyansh

---
+++ day1.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#define PI 5.89
+
+int main()
+{
+    // Code Part
+    // const int a = 78;
+    // int a = 9;
+    // int b = 9;
+    // int c = a + b;
+    // printf("Hello Program");
+    // return 0;
+
+    // int num1 = 6;
+    // printf("The value of num1 is: %d\n", num1);
+    // num1 = 78;
+    // printf("The value of num1 is: %d", num1);
+    // int divyansh = 16;
+    // int divyansh_kuwar = 89;
+    // int divyansh_kuwar345678 = 89;
+    // int break=89;
+    int aman = 78;
+    printf("%d\n", aman);
+    int AMAN = 67;
+    printf("%d\n", AMAN);
+}
