73991a35eccd752c95b5392fac268fc7c64eea72
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-12-24 15:32:31 +0530

    Break and continue

---
+++ day6.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+
+    // int table = 1;
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     printf("%d X %d = %d\n", table, i, (table * i));
+    // }
+
+    // int i = 1;
+    // while (i < 11)
+    // {
+    //     printf("%d ", i);
+    //     i++;
+    // }
+
+    // int i = 5;
+    // do
+    // {
+    //     printf("%d ", i);
+    //     i++;
+    // } while (i < 11);
+
+    // for (int i = 1; i <= 10; i++)
+    // {
+    // if (i == 5)
+    // {
+    //     break;
+    // }
+
+    // if (i == 5)
+    // {
+    //     continue;
+    // }
+    // printf("%d ", i);
+    // }
+
+    // for (int i = 1; i < 11; i++)
+    // {
+    //     if (i == 4)
+    //     {
+    //         goto label1;
+    //     }
+    //     printf("%d ", i);
+    // }
+
+    // label1:
+    //     printf("\nHello Good Morning");
+    //     goto label3;
+    // label2:
+    //     printf("\nHello Good AfterNoon");
+    // label3:
+    //     printf("\nHello Good Evening");
+
+    // TypeCasting in C language
+
+    // int a = 6;
+    // int b = 5;
+    // float result = (float)a / b;
+
+    // printf("%f", result);
+
+    return 0;
+}
